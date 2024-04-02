c319fd49a9b01901a89ed08849eda465eb6713fe
Sanaz <szeraati69@gmail.com>
2024-04-02 23:10:26 +0330

    fetch data and show current weather

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("start");
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "c980112b93d5cba99d99d53547360af3";
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+const weatherContainer = document.getElementById("weather");
+
+const getCurrentWeatherByName = async (city) => {
+  const url = `${BASE_URL}//weather?q=${city}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
+const renderCurrentWeather = (data) => {
+  console.log(data);
+  const weatherJSx = `
+  <h1>${data.name}, ${data.sys.country}</h1>
+  <div id="main">
+    <img
+      alt="weather icon"
+      src="https://api.openweathermap.org/img/w/${data.weather[0].icon}.png"
+    />
+    <span>${data.weather[0].main}</span>
+    <p>${Math.round(data.main.temp)} °C</p>
+  </div>
+  <div id="info">
+  <p>Humidity: <span>${data.main.humidity}</span></p>
+  <p>Wind Speed: <span>${data.wind.speed} m/s</span></p>
+  </div>`;
+  weatherContainer.innerHTML = weatherJSx;
+};
+
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+  if (!cityName) {
+    alert("Please enter city name");
+  }
+  const currentData = await getCurrentWeatherByName(cityName);
+  renderCurrentWeather(currentData);
+};
+
+searchButton.addEventListener("click", searchHandler);
